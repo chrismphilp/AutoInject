25ad6abcedc5b1d16642e49bdb614e6a028491a9
chrismphilp <chrismphilp@hotmail.co.uk>
2017-11-09 12:32:06 +0000

    Test output

--- AutoInject/bin/get_Packages.py
+++ AutoInject/bin/get_Packages.py
@@ -2,7 +2,7 @@ from subprocess import check_output
 import shlex
 
 out = check_output(["dpkg-query", "-W", 
-    "-f=${binary:Package}\n{binary:Package}\t${Version}\t${Architecture}\n"])
+    "-f=${binary:Package}\t${Version}\t${Architecture}\n"])
 out = out.split('\n')
 out = out.split('\t')
 print(out)
