a8b9da4962287613cddb156f9af0e8952d49ca55
chrismphilp <chrismphilp@hotmail.co.uk>
2017-11-09 12:36:36 +0000

    Test output

--- AutoInject/bin/get_Packages.py
+++ AutoInject/bin/get_Packages.py
@@ -3,6 +3,5 @@ import shlex
 
 out = check_output(["dpkg-query", "-W", 
     "-f=${binary:Package}\t${Version}\t${Architecture}\n"])
-for line in out:
-    print(line)
-    print()
+print(type(out))
+print(out)
