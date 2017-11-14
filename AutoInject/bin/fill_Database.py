bb0b8c8ef499ac87ccaf5bc9add19b85150dc77c
chrismphilp <chrismphilp@hotmail.co.uk>
2017-11-14 18:33:37 +0000

    mongo

--- AutoInject/bin/fill_Database.py
+++ AutoInject/bin/fill_Database.py
@@ -8,7 +8,7 @@ cve_data    = db.cves
 
 count = 0
 
-for data in cve_data.find({"vulnerable_configuration": { '$regex' : "windows" }}):
+for data in cve_data.find({"vulnerable_configuration": { '$regex' : "microsoft:windows_98" }}):
     if (count > 4): break
     pprint.pprint(data)
     count += 1
