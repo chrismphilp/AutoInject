8c3d41efcbef5dee88422b2a565d3ea767731d92
christopher philp <chrismphilp@hotmail.co.uk>
2017-11-14 17:46:47 +0000

    Mongo

--- AutoInject/bin/fill_Database.py
+++ AutoInject/bin/fill_Database.py
@@ -2,8 +2,9 @@ import pymongo
 from pymongo import MongoClient
 import pprint
 
-client  = MongoClient()
-db      = client['cvedb']
-
+client      = MongoClient()
+db          = client['cvedb']
 cve_data    = db.cves
-pprint.pprint(cve_data.find_one())
+
+for data in cve_data.find({"vulnerable_configuration": "ubuntu"}):
+    pprint.pprint(data)
