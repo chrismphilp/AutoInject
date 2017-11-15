0e80b8614a594380ad40bed993adeac70dc9955c
christopher philp <chrismphilp@hotmail.co.uk>
2017-11-15 18:31:55 +0000

    Sorting packages

--- AutoInject/bin/get_Packages.py
+++ AutoInject/bin/get_Packages.py
@@ -2,40 +2,37 @@ import pymongo
 from pymongo import MongoClient
 from subprocess import check_output
 
-client          = MongoClient()
-db              = client['package_db']
-collection      = db['package_list']
+client                  = MongoClient()
+db                      = client['package_db']
+collection              = db['package_list']
 
-out             = check_output(["dpkg-query", "-W", "-f=${binary:Package}\t${Version}\t${Architecture}\n"])
-tmp             = out.split('\n')
-listOfPackages  = []
-
-for line in tmp:
-    ntmp        = line.split('\t')
-    listOfPackages.append(ntmp)
-
-db.package_list.delete_many({})
-print('Collection names:', db.collection_names())
-
-python_data = collection.find_one({ 'package_name' : 'python' })
-print(python_data)
-
-for package_array in listOfPackages:
+def get_Packages():
     
-    try:
-        package_item = {
-            'package_name' : package_array[0],
-            'version' : package_array[1],
-            'architecture' : package_array[2]
-        }
-    
-    except:
-        print("Error inserting", package_array)
-        continue
-    
-    result = collection.insert_one(package_item)
+    out                 = check_output(["dpkg-query", "-W", "-f=${binary:Package}\t${Version}\t${Architecture}\n"])
+    tmp                 = out.split('\n')
 
-print('Finished inserting into DB')
+    # Deleting any current package details
+    db.package_list.drop()
+    print('Collection names:', db.collection_names())
+    
+    for line in tmp:
+        package_array   = line.split('\t')
 
-python_data = collection.find_one({ 'package_name' : 'python' })
-print(python_data)
+        try:
+            package_item = {
+                'package_name' : package_array[0],
+                'version' : package_array[1],
+                'architecture' : package_array[2]
+            }
+        
+        except:
+            print("Error inserting", package_array)
+            continue
+        
+        result = collection.insert_one(package_item)
+
+    print('Finished inserting into DB')
+    python_data = collection.find_one({ 'package_name' : 'python' })
+    print(python_data)
+
+get_Packages()
--- AutoInject/bin/views.py
+++ AutoInject/bin/views.py
@@ -1,5 +1,7 @@
+import pymongo
 from AutoInject import app
 from flask import Flask, render_template, request
+from pymongo import MongoClient
 
 @app.route("/")
 def index():
