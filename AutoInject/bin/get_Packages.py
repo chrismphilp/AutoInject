a20b6619d8b7893ed52a2071f8a8df0b21417c38
christopher philp <chrismphilp@hotmail.co.uk>
2017-11-15 18:13:12 +0000

    Sorting packages

--- AutoInject/bin/get_Packages.py
+++ AutoInject/bin/get_Packages.py
@@ -14,14 +14,16 @@ for line in tmp:
     ntmp        = line.split('\t')
     listOfPackages.append(ntmp)
 
+db.package_list.delete_many({})
 print(db.collection_names())
 
+
 for package_array in listOfPackages:
+    
     # print('Package name:', package_array[0])
     # print('Version:', package_array[1])
     # print('Architecture:', package_array[2])
     # print()
-
     
     try:
         package_item = {
@@ -33,8 +35,10 @@ for package_array in listOfPackages:
     except:
         continue
     
-
     result = collection.insert_one(package_item)
-    print('Result ID', result.inserted_id)
+    # print('Result ID', result.inserted_id)
 
 print('Finished inserting into DB')
+
+python_data = collection.find_one({ 'package_name' : 'python' })
+print(python_data)
