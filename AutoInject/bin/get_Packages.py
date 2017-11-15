59c2939156cbecbed44e737bc0185a144c41b973
christopher philp <chrismphilp@hotmail.co.uk>
2017-11-15 20:57:00 +0000

    Datatables

--- AutoInject/bin/get_Packages.py
+++ AutoInject/bin/get_Packages.py
@@ -1,4 +1,5 @@
-import pymongo, json
+import pymongo
+from json import loads
 from bson.json_util import dumps
 from pymongo import MongoClient
 from subprocess import check_output
@@ -39,4 +40,4 @@ def insert_Packages():
 def get_Packages_JSON():
 
     package_JSON = collection.find({})
-    return dumps(package_JSON)
+    return loads(dumps(package_JSON))
