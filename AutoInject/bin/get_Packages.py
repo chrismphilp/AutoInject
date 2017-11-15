9a9ab00814d2457baa4d32bdcd1e2ea10c7ddcd2
christopher philp <chrismphilp@hotmail.co.uk>
2017-11-15 17:50:25 +0000

    Sorting packages

--- AutoInject/bin/get_Packages.py
+++ AutoInject/bin/get_Packages.py
@@ -1,10 +1,20 @@
+import pymongo
+from pymongo import MongoClient
 from subprocess import check_output
-import shlex
+
+client          = MongoClient()
+db              = client['package_db']
+collection      = db['package_list']
 
 out             = check_output(["dpkg-query", "-W", "-f=${binary:Package}\t${Version}\t${Architecture}\n"])
 tmp             = out.split('\n')
 listOfPackages  = []
 
 for line in tmp:
-    ntmp = line.split('\t')
+    ntmp        = line.split('\t')
     listOfPackages.append(ntmp)
+
+for package_array in listOfPackages:
+    print('Package name:', package_array[0])
+    print('Version:', package_array[1])
+    print('Architecture:', package_array[2])
