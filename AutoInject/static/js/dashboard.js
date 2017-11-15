80e31b3787c7d6379c5b419f211bdd8837320a7d
christopher philp <chrismphilp@hotmail.co.uk>
2017-11-15 19:25:36 +0000

    Datatables

--- AutoInject/static/js/dashboard.js
+++ AutoInject/static/js/dashboard.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-    $('#packages').DataTable();
+    $('#packages').DataTable( {
+        "paging":   true,
+        "ordering": true,
+        "order": [[ 1, "desc" ]],
+        "info":     true
+    } );
 } );
+
+var table = $('#packages').DataTable();
+
+table.row.add( {
+       "Tiger Nixon",
+       "System Architect",
+       "$3,120",
+} ).draw();
