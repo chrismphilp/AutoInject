6255b512fa4d3dce1e0861527115074ec94b7352
christopher philp <chrismphilp@hotmail.co.uk>
2017-11-16 14:35:20 +0000

    Datatables

--- AutoInject/static/js/dashboard.js
+++ AutoInject/static/js/dashboard.js
@@ -1,13 +1,23 @@
 $(document).ready(function() {
     $('#packages').DataTable( {
-        "paging":   true,
-        "ordering": true,
-        "order": [[ 1, "desc" ]],
-        "info":     true,
+        "paging":       true,
+        "ordering":     true,
+        "order":        [[ 1, "desc" ]],
+        "info":         true,
         "columns": [
             { data: "package_name" },
             { data: "package_version" },
             { data: "architecture" }
         ]  
     } );
+
+    var table = $('#packages').DataTable();
+    // var JSON_data = {{ package_JSON_data }};
+
+    table.row.add( [
+        data.package_name,
+        data.version,
+        data.architecture
+    ] ).draw( false );
+
 } );
