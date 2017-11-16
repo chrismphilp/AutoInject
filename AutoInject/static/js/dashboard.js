5ade6b082e4c4aa0d73d134428cfcd2b332cbfea
chrismphilp <chrismphilp@hotmail.co.uk>
2017-11-16 00:22:44 +0000

    Working DataTables for User Packages

--- AutoInject/static/js/dashboard.js
+++ AutoInject/static/js/dashboard.js
@@ -1,20 +1,10 @@
 $(document).ready(function() {
     $('#packages').DataTable( {
-        "lengthMenu":   [ 10, 25 ],
+        "lengthMenu":   [ 10, 15 ],
         "paging":       true,
         "processing":   true,
         "ordering":     true,
-        "order":        [[ 1, "desc" ]],
+        "order":        [[ 0, "asc" ]],
         "info":         true
     } );
-
-    var table = $('#packages').DataTable();
-    // // var JSON_data = {{ package_JSON_data }};
-
-    table.row.add( [
-        "Hello",
-        "Hola",
-        "Senor"
-    ] ).draw();
-
 } );
