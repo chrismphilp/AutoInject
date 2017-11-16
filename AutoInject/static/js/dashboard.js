b4c28cc295c3884e059c276fbf3f27308323fc7e
christopher philp <chrismphilp@hotmail.co.uk>
2017-11-16 15:15:13 +0000

    Datatables

--- AutoInject/static/js/dashboard.js
+++ AutoInject/static/js/dashboard.js
@@ -1,12 +1,5 @@
 $(document).ready(function() {
     $('#packages').DataTable( {
-        "columnDefs": [
-            { "name": "engine",   "targets": 0 },
-            { "name": "browser",  "targets": 1 },
-            { "name": "platform", "targets": 2 },
-            { "name": "version",  "targets": 3 },
-            { "name": "grade",    "targets": 4 }
-        ],
         "lengthMenu":   [ 10, 25 ],
         "paging":       true,
         "processing":   true,
